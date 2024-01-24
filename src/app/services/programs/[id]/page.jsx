6797538a2dba708328e51cfec5f71621c435f9c8
Igor Shokalo <igor.shokalo78@gmail.com>
2024-01-24 14:53:08 +0200

    removed react-file-viewer

--- src/app/services/programs/[id]/page.jsx
+++ src/app/services/programs/[id]/page.jsx
@@ -2,15 +2,6 @@
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import Programs from '@/components/Programs/Programs';
-// import { strapiApi } from '@/api';
-
-// export async function generateStaticParams() {
-//   const programsList = await strapiApi.get(`/api/courses?populate=*`);
-
-//   return programsList.data.data.map((post) => ({
-//     id: post.id.toString(),
-//   }));
-// }
 
 const ServicesProgramsList = ({ params }) => {
   const { id } = params;
