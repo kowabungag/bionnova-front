d90a27438e6333fef62e931bac2e059f97d19bf7
Igor Shokalo <igor.shokalo78@gmail.com>
2024-01-24 15:07:22 +0200

    added new url

--- src/api/strapiAxios.js
+++ src/api/strapiAxios.js
@@ -1,5 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
-const strapiAxios = axios.create({baseURL: process.env.NEXT_PUBLIC_STRAPI_BACKEND})
+// const strapiAxios = axios.create({baseURL: process.env.NEXT_PUBLIC_STRAPI_BACKEND})
+
+const strapiAxios = axios.create({ baseURL: 'https://api-bioinnova.onrender.com/' });
 
 export default strapiAxios;
