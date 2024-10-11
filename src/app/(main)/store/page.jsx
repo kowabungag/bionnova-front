951b6e69f62815f1b437fef8167df2e52614b0e7
kowabungag <a.stashko23@gmail.com>
2024-10-11 11:53:48 +0300

    add class

--- src/app/(main)/store/page.jsx
+++ src/app/(main)/store/page.jsx
@@ -60,6 +60,7 @@ class App extends React.Component {
                 key={product.id}
                 product={product}
                 onAdd={this.addToOrder}
+                items={this.state.items}
               />
             ))}
           </div>
