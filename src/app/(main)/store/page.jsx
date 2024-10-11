388ccc7790d06f922ce662fc9d1a4bf0d7ee906f
kowabungag <a.stashko23@gmail.com>
2024-10-11 11:31:54 +0300

    add class

--- src/app/(main)/store/page.jsx
+++ src/app/(main)/store/page.jsx
@@ -1,58 +1,84 @@
-'use client';
-import * as styles from './store.module.scss';
-import Container from '@/components/Container/Container';
-import StoreCard from '@/components/StoreCard/StoreCard';
+"use client";
+import * as styles from "./store.module.scss";
+import Container from "@/components/Container/Container";
+import StoreCard from "@/components/StoreCard/StoreCard";
+import React from "react";
 
 const storeProducts = [
   {
     id: 1,
     title:
-      'Чем запить таблетку? Фармацевт о том, почему нельзя делить таблетки на части, хранить их на кухне',
-    author: 'Кристин Гиттер',
-    img: '../../../media/img/book-1.png',
+      "Чем запить таблетку? Фармацевт о том, почему нельзя делить таблетки на части, хранить их на кухне",
+    author: "Кристин Гиттер",
+    img: "../../../media/img/book-1.png",
   },
   {
     id: 2,
     title:
-      'Чем запить таблетку? Фармацевт о том, почему нельзя делить таблетки на части, хранить их на кухне',
-    author: 'Кристин Гиттер',
-    img: '../../../media/img/book-1.png',
+      "Чем запить таблетку? Фармацевт о том, почему нельзя делить таблетки на части, хранить их на кухне",
+    author: "Кристин Гиттер",
+    img: "../../../media/img/book-1.png",
   },
   {
     id: 3,
     title:
-      'Чем запить таблетку? Фармацевт о том, почему нельзя делить таблетки на части, хранить их на кухне',
-    author: 'Кристин Гиттер',
-    img: '../../../media/img/book-1.png',
+      "Чем запить таблетку? Фармацевт о том, почему нельзя делить таблетки на части, хранить их на кухне",
+    author: "Кристин Гиттер",
+    img: "../../../media/img/book-1.png",
   },
   {
     id: 4,
     title:
-      'Чем запить таблетку? Фармацевт о том, почему нельзя делить таблетки на части, хранить их на кухне',
-    author: 'Кристин Гиттер',
-    img: '../../../media/img/book-1.png',
+      "Чем запить таблетку? Фармацевт о том, почему нельзя делить таблетки на части, хранить их на кухне",
+    author: "Кристин Гиттер",
+    img: "../../../media/img/book-1.png",
   },
   {
     id: 5,
     title:
-      'Чем запить таблетку? Фармацевт о том, почему нельзя делить таблетки на части, хранить их на кухне',
-    author: 'Кристин Гиттер',
-    img: '../../../media/img/book-1.png',
+      "Чем запить таблетку? Фармацевт о том, почему нельзя делить таблетки на части, хранить их на кухне",
+    author: "Кристин Гиттер",
+    img: "../../../media/img/book-1.png",
   },
 ];
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      orders: [],
+      items: [],
+    };
+    this.addToOrder = this.addToOrder.bind(this);
+  }
+  render() {
+    return (
+      <>
+        <Container sizeZero>
+          <div className={styles.cardsWrapper}>
+            {storeProducts.map((product) => (
+              <StoreCard key={product.id} product={product} />
+            ))}
+          </div>
+        </Container>
+      </>
+    );
+  }
+  addToOrder(item) {}
+}
+export default App;
 
-const StorePage = () => {
-  return (
-    <>
-      <Container sizeZero>
-        <div className={styles.cardsWrapper}>
-          {storeProducts.map((product) => (
-            <StoreCard key={product.id} product={product} />
-          ))}
-        </div>
-      </Container>
-    </>
-  );
-};
+// const StorePage = () => {
+//   return (
+//     <>
+//       <Container sizeZero>
+//         <div className={styles.cardsWrapper}>
+//           {storeProducts.map((product) => (
+//             <StoreCard key={product.id} product={product} />
+//           ))}
+//         </div>
+//       </Container>
+//     </>
+//   );
+// };
 
-export default StorePage;
+// export default StorePage;
