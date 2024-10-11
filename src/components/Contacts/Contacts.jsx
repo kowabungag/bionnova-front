a1aa2de4759e52c36e5e4d349a620ba1e810cd33
kowabungag <a.stashko23@gmail.com>
2024-10-11 14:16:12 +0300

    add class

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,41 +1,37 @@
-'use client'
-import React, {useEffect} from 'react';
+"use client";
+import React, { useEffect } from "react";
 // styles
 import * as styles from "./contacts.module.scss";
 import Container from "../Container/Container";
 import ContactForm from "../UI/ContactForm/ContactForm";
-import {scrollToTop} from "../../helpers/scrollToTop";
-import {useTranslation} from "react-i18next";
-
-
+import { scrollToTop } from "../../helpers/scrollToTop";
+import { useTranslation } from "react-i18next";
 
 const Contacts = () => {
-    const {t} = useTranslation();
-    useEffect(() => {
-        scrollToTop()
-    },)
+  const { t } = useTranslation();
+  useEffect(() => {
+    scrollToTop();
+  });
 
-    return (
+  return (
     <div className={styles.root}>
-        <Container additionalPadding>
-            <div className={styles.row}>
-                {/*<div className={`${styles.column} ${styles.columnLeft}`}>*/}
-                    <div className={styles.topInfo}>
-                        <p className={styles.title}>
-                            {t('additional.contacts')}
-                        </p>
-                        <p className={styles.subtitle}>
-                            {t('additional.contactsDescription')}
-                        </p>
-                    </div>
-                {/*</div>*/}
-                <div className={`${styles.column} ${styles.columnRight}`}>
-                    <ContactForm/>
-                </div>
-            </div>
-        </Container>
+      <Container additionalPadding>
+        <div className={styles.row}>
+          {/*<div className={`${styles.column} ${styles.columnLeft}`}>*/}
+          <div className={styles.topInfo}>
+            <p className={styles.title}>{t("additional.contacts")}</p>
+            <p className={styles.subtitle}>
+              {t("additional.contactsDescription")}
+            </p>
+          </div>
+          {/*</div>*/}
+          <div className={`${styles.column} ${styles.columnRight}`}>
+            <ContactForm />
+          </div>
+        </div>
+      </Container>
     </div>
-    );
+  );
 };
 
 export default Contacts;
